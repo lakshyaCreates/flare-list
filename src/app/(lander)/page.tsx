8d5500c37f996bb3b3a79db6574fc3d84b4cfe16
lakshyaCreates <lakshya.creates07@gmail.com>
2025-02-06 10:58:47 +0530

    feat: add particles to lander

--- src/app/(lander)/page.tsx
+++ src/app/(lander)/page.tsx
@@ -1,8 +1,18 @@
+import { Particles } from "@/components/ui/particles";
+
 import { Hero } from "./components/hero";
 
 export default function HomePage() {
     return (
         <div className="space-y-12">
+            <Particles
+                className="absolute inset-0"
+                quantity={60}
+                ease={85}
+                color={"#6d28d9"}
+                refresh
+                size={0.8}
+            />
             <Hero />
         </div>
     );
