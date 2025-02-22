515de7e67ca7e151475250eb39b798e49311c577
lakshyaCreates <lakshya.creates07@gmail.com>
2025-02-22 12:45:15 +0530

    feat: setup drizzle & t3-oss env

---
+++ drizzle/0000_gigantic_iron_fist.sql
@@ -0,0 +1,8 @@
+CREATE TABLE "user" (
+	"id" text PRIMARY KEY NOT NULL,
+	"name" text,
+	"email" text,
+	"emailVerified" timestamp,
+	"image" text,
+	CONSTRAINT "user_email_unique" UNIQUE("email")
+);
